3a4a79cb3fc7aadae699ad634b15e000e3e4992d
Markel <mitmoto1645@gmail.com>
2024-02-22 16:51:13 +0300

    compliete

--- asinh.go
+++ asinh.go
@@ -1,22 +1,44 @@
 package main
 
-import (
-	"fmt"
-	// "time"
-)
+// import (
+// 	// "fmt"
+// 	// "time"
+// )
 
 func main() {
 
 	// Асинхроннность / рутины
-	ch := make(chan int)
 
-	go say("Hello Go", ch)
-	// time.Sleep(2 * time.Second)
-	fmt.Println(<-ch)
+	// ch := make(chan string)
+	// ch1 := make(chan int)
 
+	// go say("Hello Go", ch, ch1)
+	// // time.Sleep(2 * time.Second)
+	// data := <-ch
+	// fmt.Println(data, <-ch1)
+
+	// ch := make(chan int)
+
+	// go say("Hello Go", ch)
+
+	// for a := range ch {
+	// 	fmt.Println(a)
+	// }
+	// fmt.Println(<-ch)
 }
 
-func say(greet string, ch chan int) {
-	fmt.Println(greet)
-	ch <- 7
-}
+// func say(greet string, ch chan string, ch1 chan int) {
+// 	fmt.Println(greet)
+// 	ch <- "Hello"
+// 	ch1 <- 77
+
+// }
+
+// func say(greet string, ch chan int) {
+// 	for i := 0; i <= 5; i++ {
+// 		ch <- i
+// 	}
+
+// 	close(ch) // Закрытие канала
+
+// }
--- wwf.go
+++ wwf.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
+	// "os"
 )
 
 func main() {
@@ -10,22 +10,49 @@ func main() {
 
 	// Работа с файлами
 
-	data := []byte("Text to file")
+	// data := []byte("Text to file")
 
-	e := os.WriteFile("text.txt", data, 0600)
+	// e := os.WriteFile("text.txt", data, 0600)
 
-	if e != nil {
-		fmt.Println("Не могу создать файл\n", e)
+	// if e != nil {
+	// 	fmt.Println("Не могу создать файл\n", e)
+	// }
+
+	// file_data, err := os.ReadFile("text.txt") // Открытие файла
+
+	// if err != nil { // Проверка на ошибку
+	// 	fmt.Println("Не могу прочитать фаил\n", err)
+	// }
+
+	// fmt.Println(string(file_data))
+
+	// os.Remove("text.txt")
+
+	// file, err := os.Create("text.txt")
+
+	// if err != nil {
+	// 	println("Ошибка", err)
+	// 	os.Exit(1)
+	// }
+	// defer file.Close()
+
+	// data := "Text to file"
+
+	// file.WriteString(data)
+
+	// file_data, _ := os.ReadFile(file.Name())
+
+	// fmt.Println(string(file_data))
+
+	i := 0
+LOOP:
+	if i > 50 {
+		goto END
 	}
 
-	file_data, err := os.ReadFile("text.txt") // Открытие файла
+	fmt.Println(i)
+	i++
+	goto LOOP
 
-	if err != nil { // Проверка на ошибку
-		fmt.Println("Не могу прочитать фаил\n", err)
-	}
-
-	fmt.Println(string(file_data))
-
-	os.Remove("text.txt")
-
+END:
 }
